1ed38aa6547d579bd75db6183303c16264ef5c23
th1nkp4n <kqi12345@gmail.com>
2019-08-08 14:57:46 -0400

    add working start, sync_state, and terminate

--- examples/particle/aws/step_functions/sfsmtest.py
+++ examples/particle/aws/step_functions/sfsmtest.py
@@ -1,55 +1,65 @@
 from pcf.particle.aws.step_functions.state_machine import StateMachine
 from pcf.core import State
+import json
+import time
+
+state_machine_definition = {
+    "Comment": "using an existing step function defintion to test pcf",
+    "StartAt": "Start Task And Wait For Callback",
+    "States": {
+        "Start Task And Wait For Callback": {
+        "Type": "Task",
+        "Resource": "arn:aws:states:::sqs:sendMessage.waitForTaskToken",
+        "Parameters": {
+            "QueueUrl": "https://sqs.us-east-1.amazonaws.com/471176887411/StepFunctionsTemplate-CallbackQueue",
+            "MessageBody": {
+                "MessageTitle": "Task started by Step Functions. Waiting for callback with task token.",
+                "TaskToken.$": "$$.Task.Token"
+            }
+        },
+        "Next": "Notify Success",
+        "Catch": [
+        {
+            "ErrorEquals": [ "States.ALL" ],
+            "Next": "Notify Failure"
+        }
+        ]
+        },
+        "Notify Success": {
+        "Type": "Task",
+        "Resource": "arn:aws:states:::sns:publish",
+        "Parameters": {
+            "Message": "Callback received. Task started by Step Functions succeeded.",
+            "TopicArn": "arn:aws:sns:us-east-1:471176887411:StepFunctionsSample-WaitForCallbacke1585b4e-1900-4c23-bbbf-959f3161fd6e-SNSTopic-1H9M0JROHKHX2"
+        },
+        "End": True
+        },
+        "Notify Failure": {
+        "Type": "Task",
+        "Resource": "arn:aws:states:::sns:publish",
+        "Parameters": {
+            "Message": "Task started by Step Functions failed.",
+            "TopicArn": "arn:aws:sns:us-east-1:471176887411:StepFunctionsSample-WaitForCallbacke1585b4e-1900-4c23-bbbf-959f3161fd6e-SNSTopic-1H9M0JROHKHX2"
+        },
+        "End":True
+        }
+    }
+}
+
 
 state_machine_json = {
     "pcf_name" : "pcf-sm-test",
     "flavor" : "stepfunctions",
     "aws_resource" : {
         "name" : "pcf-sm-test",
-        "definition" : {
-            "Comment": "using an existing step function defintion to test pcf",
-            "StartAt": "Start Task And Wait For Callback",
-            "States": {
-                "Start Task And Wait For Callback": {
-                "Type": "Task",
-                "Resource": "arn:aws:states:::sqs:sendMessage.waitForTaskToken",
-                "Parameters": {
-                    "QueueUrl": "https://sqs.us-east-1.amazonaws.com/471176887411/StepFunctionsTemplate-CallbackQueue",
-                    "MessageBody": {
-                        "MessageTitle": "Task started by Step Functions. Waiting for callback with task token.",
-                        "TaskToken.$": "$$.Task.Token"
-                    }
-                },
-                "Next": "Notify Success",
-                "Catch": [
-                {
-                    "ErrorEquals": [ "States.ALL" ],
-                    "Next": "Notify Failure"
-                }
-                ]
-                },
-                "Notify Success": {
-                "Type": "Task",
-                "Resource": "arn:aws:states:::sns:publish",
-                "Parameters": {
-                    "Message": "Callback received. Task started by Step Functions succeeded.",
-                    "TopicArn": "arn:aws:sns:us-east-1:471176887411:StepFunctionsSample-WaitForCallbacke1585b4e-1900-4c23-bbbf-959f3161fd6e-SNSTopic-1H9M0JROHKHX2"
-                },
-                "End": True
-                },
-                "Notify Failure": {
-                "Type": "Task",
-                "Resource": "arn:aws:states:::sns:publish",
-                "Parameters": {
-                    "Message": "Task started by Step Functions failed.",
-                    "TopicArn": "arn:aws:sns:us-east-1:471176887411:StepFunctionsSample-WaitForCallbacke1585b4e-1900-4c23-bbbf-959f3161fd6e-SNSTopic-1H9M0JROHKHX2"
-                },
-                "End":True
-                }
-            }
-        },
+        "definition" : json.dumps(state_machine_definition),
         "roleArn" : "arn:aws:iam::471176887411:role/StepFunctionsSample-WaitForCal-StatesExecutionRole-GONJVXVYABNM", 
-        "tags" : {}
+        "tags" : [
+            {
+                "key" : "OwnerContact",
+                "value": "kyla.qi@capitalone.com"
+            }
+        ]
     }
 }
 
@@ -58,5 +68,12 @@ particle = StateMachine(state_machine_json)
 particle.set_desired_state(State.running)
 particle.apply()
 
+print(particle.get_state())
+
+time.sleep(60)
+
+particle.set_desired_state(State.terminated)
+particle.apply()
 
 print(particle.get_state())
+
--- pcf/particle/aws/step_functions/state_machine.py
+++ pcf/particle/aws/step_functions/state_machine.py
@@ -34,9 +34,13 @@ class StateMachine(AWSResource):
     }
 
     UPDATE_PARAM_FILTER = {
-        "",
-        "Definition",
-        "RoleArn"
+        "stateMachineArn",
+        "definition",
+        "roleArn"
+    }
+
+    TAG_PARAM_FILTER = {
+        "tags"
     }
 
     UNIQUE_KEYS = ["aws_resource.name"]
@@ -47,13 +51,21 @@ class StateMachine(AWSResource):
 
     def sync_state(self):
         try:
-            self.state= self.get_status()
+            current_description = self.get_state_machine()
+            if current_description is None:
+                self.state= StateMachine.state_lookup.get('missing')
+            else: 
+                self.state = StateMachine.state_lookup.get(current_description["status"].lower()) 
+                self.current_state_definition["name"] = current_description["name"]
+                self.current_state_definition["definition"] = current_description["definition"]
+                self.current_state_definition["roleArn"] = current_description["roleArn"]
+                self.current_state_definition["tags"] = self.client.list_tags_for_resource(resourceArn = self._arn)["tags"]
         except NoResourceException:
             self.state = StateMachine.state_lookup.get('missing')
 
 
     def _terminate(self):
-        resp = self.client.delete_activity(activityArn=self._arn)
+        resp = self.client.delete_state_machine(stateMachineArn=self._arn)
 
     def _start(self):
         """
@@ -62,9 +74,11 @@ class StateMachine(AWSResource):
             response of boto3 create_activity
         """
         start_definition = pcf_util.param_filter(self.get_desired_state_definition(), StateMachine.START_PARAM_FILTER)
-        print(start_definition)
+        #print(start_definition)
         response = self.client.create_state_machine(**start_definition)
-        self._arn = response.stateMachineArn
+        #print(response)
+        self._arn = response["stateMachineArn"]
+        print(self.arn)
         return response
 
     def _stop(self):
@@ -75,20 +89,27 @@ class StateMachine(AWSResource):
 
     def _update(self):
         update_definition = pcf_util.param_filter(self.get_desired_state_definition(), StateMachine.UPDATE_PARAM_FILTER)
-        return self.client.update_state_machine(**update_definition)
+        update_definition["stateMachineArn"] = self._arn
+        update_response = self.client.update_state_machine(**update_definition)
+        tag_response = self.client.tag_resource(resourceArn=self._arn, tags=self.desired_state_definition["tags"])
+        return update_response
     
-    def get_status(self):
+    def get_state_machine(self):
         try:
             if self._arn is not None:
                 current_description = self.client.describe_state_machine(stateMachineArn= self._arn)
-                print(current_description)
-                current_status = current_description.status
             else: 
-                current_status = State.terminated
+                response = self.client.list_state_machines()
+                current_description = None
+                for machine in response["stateMachines"]:
+                    if machine["name"] == self.machine_name:
+                        self._arn = machine["stateMachineArn"]
+                        current_description = self.client.describe_state_machine(stateMachineArn= machine["stateMachineArn"])
+            return current_description
         except ClientError as e:
-            if e.response['Error']['Code'] == 'ResourceNotFoundException':
-                logger.info("Function {} was not found. State is terminated".format(self.machine_name))
-                return State.terminated
+            if e.response['Error']['Code'] == 'StateMachineDoesNotExist':
+                logger.info("State machine {} was not found. State is terminated".format(self.machine_name))
+                return None
             else:
-                raise e
-        return current_status
+                raise e    
+    
